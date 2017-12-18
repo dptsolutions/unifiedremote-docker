ff54123d469445d7c2545cd5c396fcc500cf5f12
Unknown <mogul345@gmail.com>
2017-12-17 16:51:49 -0800

    Got to be in the proper directory to use relative paths. Doh!

--- etc/my_init.d/00-install-ur.sh
+++ etc/my_init.d/00-install-ur.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
 
 mkdir /urserver
-echo Downloading Unified Remote Server from http://www.unifiedremote.com/d/linux-x64-portable
-curl -J -L -o /ur/urserver.tar.gz http://www.unifiedremote.com/d/linux-x64-portable
-cd /ur
-echo Untarring server files
-tar -zxvf urserver.tar.gz
-cd urserver*/
-echo Copying server files
-cp -r . /urserver
 
 if [ -e /config/urserver.config ]
 then
 	echo urserver.config already in /config, skipping copy.
 else
 	echo urserver.config not found in /config, copying default.
-	cp urserver.config /config
+	cp /ur/urserver.config /config
 fi
 
-#echo Cleaning up temporary installation files
-#rm -rf /ur
+echo Downloading Unified Remote Server from http://www.unifiedremote.com/d/linux-x64-portable
+curl -J -L -o /ur/urserver.tar.gz http://www.unifiedremote.com/d/linux-x64-portable
+
+echo Untarring server files
+cd /ur
+tar -zxvf urserver.tar.gz
+
+echo Copying server files
+cd urserver*/
+cp -r . /urserver
+
+echo Cleaning up temporary installation files
+rm -rf /ur
